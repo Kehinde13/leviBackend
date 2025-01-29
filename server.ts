0d2c218ec80474e7d9bf1b64aaba03e1837dfc52
Kehinde Balogun <balogunkehinde3@gmail.com>
2025-01-29 06:45:03 -0500

    first commit

---
+++ server.ts
@@ -0,0 +1,8 @@
+// server.ts
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+/* import userRoutes from './src/routes/userRoutes';
+import productRoutes from './src/routes/productRoutes'; */
+
+
